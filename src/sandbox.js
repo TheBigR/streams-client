7604a45ccd7d7dab94d384336b9a492192ce878b
TheBigR <nevoroy@gmail.com>
2020-12-22 09:12:08 +0200

    adding more to sandbox

--- src/sandbox.js
+++ src/sandbox.js
@@ -30,3 +30,12 @@ function processData(input) {
     }
   }
 }
+
+function reverseString(s) {
+  try {
+    console.log(s.split('').reverse().join(''))
+  } catch (error) {
+    console.log(error.message)
+    console.log(s)
+  }
+}
